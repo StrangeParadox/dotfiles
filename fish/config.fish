0ba8ae35f9564cd735899ea20ae21a2aaac6731c
StrangeParadox <d.ellis01.dje@gmail.com>
2021-03-21 22:16:52 +0100

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+starship init fish | source
+
+function ls 
+	command ls -l --group-directories-first --color $argv
+end
+
+function ll
+	command ls -l -A --group-directories-first --color $argv
+end
+
+function vim
+	command nvim $argv
+end
