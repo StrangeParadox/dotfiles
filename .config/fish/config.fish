a9221d8c63a8a392cfa12c65d5accfaf2464cd73
Dillon <61332067+StrangeParadox@users.noreply.github.com>
2021-05-13 03:41:58 +0100

    Delete .config directory

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-starship init fish | source
-
-function ls 
-	command ls -l --group-directories-first --color $argv
-end
-
-function ll
-	command ls -l -A --group-directories-first --color $argv
-end
