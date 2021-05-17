f316dc62e2423deaa8f64b58869f0c79783374e6
StrangeParadox <d.ellis01.dje@gmail.com>
2021-05-17 22:57:50 +0200

    updated dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,16 @@
+# functions
+function github
+    if fish_is_root_user
+        eval (ssh-agent -c)
+        ssh-add ~/.ssh/gh_id_25519
+
+        echo "loaded github keys!"
+    end
+end
+
 # aliases
-alias ls="ls -l -h --color --group-directories-first"
-alias ll="ls -A"
+alias ls="exa --long --header --group-directories-first"
+alias ll="ls -a"
 
 # init starship
 starship init fish | source
